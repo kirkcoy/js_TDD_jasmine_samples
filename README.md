8ad345a28973c7171e40077762f891f4950b60e6
Kirk Coyoca <klcoyoca.fh01@gmail.com>
2019-12-12 10:07:41 -0800

    Added instructions in README.md and test cases for each function.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # js_TDD_jasmine_samples
-Introduction to TDD using Jasmine
+
+### Introduction to TDD using Jasmine
+Created test cases for the functions being use.
+
+Execute the test cases:
+1. cd to javascript/  
+2. run jasmine  
---
+++ javascript/spec/functionSpec.js
@@ -0,0 +1,23 @@
+function reverseArray(arr){
+    for(var i = 0; i < Math.floor(arr.length/2); i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
+
+b = reverseArray([1,2]);
+console.log('b is', b);
+
+describe("reverseArray", function(){
+
+    it("should return [5,3,1] when [1,3,5] is passed", function(){
+        expect(reverseArray([1,3,5])).toEqual([5,3,1]);
+    });
+
+    it("should return [2,4,8,1] when [1,8,4,2] is passed", function(){
+        expect(reverseArray([1,8,4,2])).toEqual([2,4,8,1]);
+    });
+
+})
